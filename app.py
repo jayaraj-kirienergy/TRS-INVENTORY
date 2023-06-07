35d283f422128c1fca9bc19963e46c43cb3f4cf3
jayaraj-kirienergy <135829463+jayaraj-kirienergy@users.noreply.github.com>
2023-06-07 17:12:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,10 @@ import pandas as pd
 import streamlit as st
 
 st.write("TEST")
-master_path = "https://github.com/jayaraj-kirienergy/TRS-INVENTORY/blob/main/database/MASTER_DATA.xlsx"
+master_path = "https://github.com/jayaraj-kirienergy/TRS-INVENTORY/blob/main/database/MASTER_DATA.xlsx?raw=True"
+unique_path = "https://github.com/jayaraj-kirienergy/TRS-INVENTORY/blob/main/database/UNIQUE_ID.xlsx?raw=True"
+project_path = "https://github.com/jayaraj-kirienergy/TRS-INVENTORY/blob/main/database/PROJECTS.xlsx?raw=True"
+
+unique_df = pd.read_excel(unique_path)
+project_df = pd.read_excel(project_path)
 master_df = pd.read_excel(master_path)
