3827c22a12ccf9aa0a4b0c5f2c7710ef8f3301bd
jayaraj-kirienergy <135829463+jayaraj-kirienergy@users.noreply.github.com>
2023-06-07 17:35:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ if button1:
       a = "AT KIRI YARD"
       st.write(a)
       out_date = st.date_input('OUTWARD-DATE:',today)
-      out_challan = input('OUTWARD - CHALLAN :')
+      out_challan = st.text_input('OUTWARD - CHALLAN :')
       need_line.loc[ind,'CHALLAN_2'] = out_challan
       need_line.loc[ind,'OUTWARD_DATE'] = out_date
       st.dataframe(need_line)
