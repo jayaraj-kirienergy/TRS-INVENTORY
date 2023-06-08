2419a1a52329550d2609ad5d4b83ea4faccd9a10
jayaraj-kirienergy <135829463+jayaraj-kirienergy@users.noreply.github.com>
2023-06-08 13:31:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,23 +22,23 @@ if button1:
   inp_assert_id = st.text_input("ENTER THE ASSERT-NO ",)
   button3 = st.button("SUBMIT")
   if button3:
-    st.write("reached till here")
-    st.write(inp_assert_id)
-    temp_master_df = master_df.drop_duplicates(subset=['ASSERT_NO'],keep = 'last')
-    st.write("reached till here")
-    s = pd.Series(list(temp_master_df['ASSERT_NO']))
-    st.write(s)
-    if inp_assert_id in s.values:
-      ind = temp_master_df[temp_master_df['ASSERT_NO']==inp_assert_id].index.values
-      need_line = temp_master_df.loc[ind]
-      need_line['OUTWARD_DATE'] = pd.to_datetime(need_line['OUTWARD_DATE'],errors='coerce')
-      need_line['INWARD_DATE'] = pd.to_datetime(need_line['INWARD_DATE'],errors='coerce')
-      #print(need_line[['ASSERT_NO','DESCRIPTION','OUTWARD_DATE','INWARD_DATE']])
-      if need_line.loc[ind,'INWARD_DATE'].notnull().any() & need_line.loc[ind,'OUTWARD_DATE'].isnull().any():
-        a = "AT KIRI YARD"
-        st.write(a)
-        out_date = st.date_input('OUTWARD-DATE:',today)
-        out_challan = st.text_input('OUTWARD - CHALLAN :')
-        need_line.loc[ind,'CHALLAN_2'] = out_challan
-        need_line.loc[ind,'OUTWARD_DATE'] = out_date
-        st.dataframe(need_line)
+      st.write("reached till here")
+      st.write(inp_assert_id)
+      temp_master_df = master_df.drop_duplicates(subset=['ASSERT_NO'],keep = 'last')
+      st.write("reached till here")
+      s = pd.Series(list(temp_master_df['ASSERT_NO']))
+      st.write(s)
+      if inp_assert_id in s.values:
+        ind = temp_master_df[temp_master_df['ASSERT_NO']==inp_assert_id].index.values
+        need_line = temp_master_df.loc[ind]
+        need_line['OUTWARD_DATE'] = pd.to_datetime(need_line['OUTWARD_DATE'],errors='coerce')
+        need_line['INWARD_DATE'] = pd.to_datetime(need_line['INWARD_DATE'],errors='coerce')
+        #print(need_line[['ASSERT_NO','DESCRIPTION','OUTWARD_DATE','INWARD_DATE']])
+        if need_line.loc[ind,'INWARD_DATE'].notnull().any() & need_line.loc[ind,'OUTWARD_DATE'].isnull().any():
+          a = "AT KIRI YARD"
+          st.write(a)
+          out_date = st.date_input('OUTWARD-DATE:',today)
+          out_challan = st.text_input('OUTWARD - CHALLAN :')
+          need_line.loc[ind,'CHALLAN_2'] = out_challan
+          need_line.loc[ind,'OUTWARD_DATE'] = out_date
+          st.dataframe(need_line)
